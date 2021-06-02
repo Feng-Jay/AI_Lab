b3b8c8dd21ee0a9ead740591121a2560be4677ac
Feng-Jay <2652925120@qq.com>
2021-06-02 17:05:08 +0800

    BFS done

--- 8code.cpp
+++ 8code.cpp
@@ -1,57 +1,120 @@
 #include <iostream>
 #include <queue>
+#include <algorithm>
+#include <tuple>
 
 using namespace std;
 
-int martix[3][3];
-int outcome[3][3]={{1,2,3},{8,0,4},{7,6,5}};
-int dx[4]={0,0,-1,1};
-int dy[4]={1,-1,0,0};
-struct Node{
-    int x;
-    int y;
+int ini[3][3]={2,8,3,1,0,4,7,6,5};
+int tar[3][3]={1,2,3,8,0,4,7,6,5};
+int dx[4]={0,0,-1,1}; int dy[4]={1,-1,0,0};
+
+struct Martix{
+    int martix[3][3];
 };
-void Init_Martix()
+
+void init(Martix &mylist)
 {
-    cout<<"Please input martix, use 0 replace empty."<<endl;
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++)
-        cin>>martix[i][j];
+        mylist.martix[i][j]=ini[i][j];
     }
 }
 
-void Print_Martix()
+tuple<int,int> find0(int mylist[3][3])
+{
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(mylist[i][j]==0)
+            return make_tuple(i,j);
+        }
+    }
+    return make_tuple(-2,-2);
+}
+
+int test(int mylist[3][3],int tar[3][3])
+{
+    int outcome=1;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(mylist[i][j]!=tar[i][j]){
+                outcome=0;
+                return outcome;
+            }
+        }
+    }
+    return outcome;
+}
+
+int runbefore(Martix martix1,vector<Martix> before)
+{
+    for(int i=0;i<before.size();i++){
+        int outcome=1;
+        for(int m=0;m<3;m++){
+            for(int n=0;n<3;n++){
+                if(martix1.martix[m][n]!=before[i].martix[m][n]){
+                    outcome=0;
+                }
+            }
+        }
+        if(outcome) return 1;
+    }
+    return 0;
+}
+void printMartix(Martix martix1)
 {
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++)
-        cout<<martix[i][j]<<" ";
+        cout<<martix1.martix[i][j]<<" ";
         cout<<endl;
     }
 }
 
-int Ok()
+int BFS(Martix &mylist)
 {
-    int ok=1;
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            if(martix[i][j]!=outcome[i][j]){
-                ok=0;
-                return ok;
+    queue<Martix> q;
+    vector<Martix> before;
+    q.push(mylist);
+    before.push_back(mylist);
+    while(!q.empty()){
+        Martix temp=q.front();
+        q.pop();
+        // cout<<"1"<<endl;
+        // printMartix(temp);
+        if(test(temp.martix,tar)){
+            cout<<"Task done"<<endl;
+            cout<<"outcome:\n";
+            printMartix(temp);
+            return 1;
+        }
+
+        tuple<int,int>locate=find0(temp.martix);
+        for(int i=0;i<4;i++){
+            int x=get<0>(locate)+dx[i];
+            int y=get<1>(locate)+dy[i];
+            if(x>=0&&x<3&&y>=0&&y<3){
+                Martix newone;
+                for(int m=0;m<3;m++){
+                    for(int n=0;n<3;n++){
+                        newone.martix[m][n]=temp.martix[m][n];
+                    }
+                }
+                swap(newone.martix[x][y],newone.martix[get<0>(locate)][get<1>(locate)]);
+                if(!runbefore(newone,before)){
+                    q.push(newone);
+                    before.push_back(newone);
+                    cout<<"push martix:\n";
+                    printMartix(newone);
+                }
             }
         }
     }
-    return ok;
+    return 1;
 }
-
-int BFS()
-{
-    queue<Node> q;
-    
-}
-
+Martix mylist;
 int main()
 {
-    Init_Martix();
-
+    init(mylist);
+    BFS(mylist);
     return 0;
 }
